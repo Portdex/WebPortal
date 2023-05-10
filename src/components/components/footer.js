ab94ae98cfea944c5abd2bff7e8ad2ea247142b4
MubashiraIqbal <mubashiraiqbal786@gmail.com>
2023-05-10 16:23:25 +0500

    contact us added

--- src/components/components/footer.js
+++ src/components/components/footer.js
@@ -45,23 +45,23 @@ const footer= () => (
                             </ul>
                         </div>
                     </div>
-                    {/* <div className="col-md-4 col-sm-6 col-xs-1">
+                    <div className="col-md-4 col-sm-6 col-xs-1">
                         <div className="widget m-0">
-                            <h5>Newsletter</h5>
-                            <p>Signup for our newsletter to get the latest news in your inbox.</p>
-                            <form action="#" className="row form-dark" id="form_subscribe" method="post" name="form_subscribe">
-                                <div className="col text-center">
-                                    <input className="form-control" id="txt_subscribe" name="txt_subscribe" placeholder="enter your email" type="text" /> 
-                                    <Link to="" id="btn-subscribe">
-                                      <i className="arrow_right bg-color-secondary"></i>
-                                    </Link>
-                                    <div className="clearfix"></div>
-                                </div>
-                            </form>
+                            <h5>Contact Us</h5>
+                            <a href='mailto:info@portdex.ai'>
+                            <p> <i className="fa fa-fw" aria-hidden="true" title="Copy to use envelope"></i> info@portdex.ai</p>
+                            </a>
+                            <div className="social-icons">
+                                        <a href="https://www.facebook.com/Portdex/" target="_blank"><i className="socialicons fa fa-facebook fa-lg"></i></a>
+                                        <a href="https://twitter.com/portdex" target="_blank"><i className="socialicons fa fa-twitter fa-lg"></i></a>
+                                        <a href="https://www.linkedin.com/company/portdex/" target="_blank"><i className="socialicons fa fa-linkedin fa-lg"></i></a>
+                                        
+                                    </div>
+                            
                             <div className="spacer-10"></div>
-                            <small>Your email is safe with us. We don't spam.</small>
+                           
                         </div>
-                    </div> */}
+                    </div>
                 </div>
             </div>
             <div className="subfooter m-0">
